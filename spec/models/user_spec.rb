c1655a20490132549f6f51506611a3bf30bf68dd
Matt Dawson <matt.dawson@fluxfederation.com>
2018-10-23 21:08:54 +1300

    :recycle: Refactor checkbox and user spec to not use should
    As per the Better Spec guideline, you should not use 'should' when
    describing tests.
    
    Reference: http://www.betterspecs.org/#should

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  it 'should have many houses' do
+  it 'has many houses' do
     t = User.reflect_on_association(:houses)
     expect(t.macro).to eq(:has_many)
   end
