b58bb6c13b3fc9346bddca5d4923ca6ee13645d9
Matt Dawson <matt.dawson@fluxfederation.com>
2018-10-23 20:57:16 +1300

    :recycle: Refactor address spec to use subject

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,36 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Address do
-  let(:address) { create(:address, house: build(:house)) }
+  subject(:address) { create(:address, house: build(:house)) }
 
   it 'should be valid' do
-    expect(address).to be_valid
+    is_expected.to be_valid
   end
 
   it 'should be valid without second address field' do
     address.address_2 = nil
-    expect(address).to be_valid
+    is_expected.to be_valid
   end
 
   context 'should not be valid without' do
     it 'first address field' do
       address.address_1 = nil
-      expect(address).to_not be_valid
+      is_expected.to_not be_valid
     end
 
     it 'city' do
       address.city = nil
-      expect(address).to_not be_valid
+      is_expected.to_not be_valid
     end
 
     it 'state' do
       address.state = nil
-      expect(address).to_not be_valid
+      is_expected.to_not be_valid
     end
 
     it 'zip_code' do
       address.zip_code = nil
-      expect(address).to_not be_valid
+      is_expected.to_not be_valid
     end
   end
 
@@ -38,19 +38,19 @@ RSpec.describe Address do
     it 'should be valid with these zip codes' do
       %w[90000 96054 37112 99999 54221-4366 90210-7321].each do |zcode|
         address.zip_code = zcode
-        expect(address).to be_valid
+        is_expected.to be_valid
       end
     end
 
     it 'should not be valid with letters' do
       address.zip_code = 'SaMo3'
-      expect(address).to_not be_valid
+      is_expected.to_not be_valid
     end
 
     it 'should not be valid with an improper seperator' do
       ['12342 - 5462', '56331 -5672', '46621- 3677', '23456_1944'].each do |zcode|
         address.zip_code = zcode
-        expect(address).to_not be_valid
+        is_expected.to_not be_valid
       end
     end
   end
@@ -58,12 +58,12 @@ RSpec.describe Address do
   context 'state' do
     it 'should be valid which it has only 2 chars' do
       address.state = 'CA'
-      expect(address).to be_valid
+      is_expected.to be_valid
     end
 
     it 'should not be valid which it does not have only 2 chars' do
       address.state = 'WSH'
-      expect(address).to_not be_valid
+      is_expected.to_not be_valid
     end
 
     it 'should be capitiliazed after save' do
@@ -75,7 +75,7 @@ RSpec.describe Address do
 
     it 'can not be integer' do
       address.state = 3
-      expect(address).to_not be_valid
+      is_expected.to_not be_valid
     end
   end
 end
