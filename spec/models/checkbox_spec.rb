e2af25fc50693875e5eeef12cc832c5f3e52bb5e
Matt Dawson <matt.dawson@fluxfederation.com>
2018-10-23 21:08:54 +1300

    :recycle: Refactor checkbox and user spec to not use should
    As per the Better Spec guideline, you should not use 'should' when
    describing tests.
    
    Reference: http://www.betterspecs.org/#should

--- spec/models/checkbox_spec.rb
+++ spec/models/checkbox_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe Checkbox do
   subject(:checkbox) { create(:checkbox, house: build(:house)) }
 
-  it 'should be valid' do
+  it 'is valid' do
     is_expected.to be_valid
   end
 
   # All of them true because of the beautiy of Active Record <3
   # AR returns to true them except the +nil+.
   ['true', 1, nil].each do |lol|
-    it "should not be valid if it has #{lol}" do
+    it "is not valid if it has #{lol}" do
       checkbox.air_conditioning = lol
       is_expected.to be_valid
     end
--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'byebug'
 
 RSpec.describe House do
   subject(:house) { create(:house, :either) }
