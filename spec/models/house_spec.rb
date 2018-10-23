502229e690e7d1253c8fa52ee603ac3624647888
Matt Dawson <matt.dawson@fluxfederation.com>
2018-10-23 20:53:41 +1300

    :recycle: Refactor house spec to use subject

--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -2,75 +2,75 @@ require 'rails_helper'
 require 'byebug'
 
 RSpec.describe House do
-  let(:house) { create(:house, :either) }
+  subject(:house) { create(:house, :either) }
 
   it 'is valid' do
-    expect(house).to be_valid
+    is_expected.to be_valid
   end
 
   context 'rent' do
     it 'is not valid without' do
       house.rent = nil
-      expect(house).not_to be_valid
+      is_expected.not_to be_valid
     end
 
     it 'can not be negative' do
       house.rent = -1
-      expect(house).not_to be_valid
+      is_expected.not_to be_valid
     end
 
     it 'can not be string'  do
       house.rent = 'three hundred'
-      expect(house).to_not be_valid
+      is_expected.to_not be_valid
     end
 
     it 'can not be zero' do
       house.rent = 0
-      expect(house).not_to be_valid
+      is_expected.not_to be_valid
     end
   end
 
   context 'deposit' do
     it 'is valid with' do
       house.deposit = 999
-      expect(house).to be_valid
+      is_expected.to be_valid
     end
 
     it 'is valid without' do
       house.deposit = nil
-      expect(house).to be_valid
+      is_expected.to be_valid
     end
 
     it 'can not be negative' do
       house.deposit = -1
-      expect(house).not_to be_valid
+      is_expected.not_to be_valid
     end
 
     it 'can not be a string' do
       house.deposit = 'lots'
-      expect(house).not_to be_valid
+      is_expected.not_to be_valid
     end
 
     it 'can not be zero' do
       house.deposit = 0
-      expect(house).not_to be_valid
+      is_expected.not_to be_valid
     end
   end
 
   context 'preferred gender' do
     it 'is not valid without' do
       house.preferred_gender = nil
-      expect(house).not_to be_valid
+      is_expected.not_to be_valid
     end
 
     it 'is not an included preferred gender' do
       house.preferred_gender = 3
-      expect(house).not_to be_valid
+      is_expected.not_to be_valid
     end
 
     it 'is an included preferred gender' do
       house.preferred_gender = 2
-      expect(house).to be_valid
+      is_expected.to be_valid
     end
     # XXX:
     #   ActiveRecord or something returns string to zero.
@@ -82,22 +82,22 @@ RSpec.describe House do
   context 'available at' do
     it 'is not valid if time is past' do
       house.available_at = 1.week.ago
-      expect(house).not_to be_valid
+      is_expected.not_to be_valid
     end
 
     it 'is valid if time is today' do
       house.available_at = Time.zone.today
-      expect(house).to be_valid
+      is_expected.to be_valid
     end
 
     it 'is valid if time is future' do
       house.available_at = 1.week.after
-      expect(house).to be_valid
+      is_expected.to be_valid
     end
 
     it 'is not valid without' do
       house.available_at = nil
-      expect(house).not_to be_valid
+      is_expected.not_to be_valid
     end
   end
 end
