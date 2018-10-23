bed636d87c8383cc952cba40d00254da23113170
Matt Dawson <matt.dawson@fluxfederation.com>
2018-10-23 20:54:32 +1300

    :recycle: Refactor checkbox spec to use subject

--- spec/models/checkbox_spec.rb
+++ spec/models/checkbox_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Checkbox do
-  let(:checkbox) { create(:checkbox, house: build(:house)) }
+  subject(:checkbox) { create(:checkbox, house: build(:house)) }
 
   it 'should be valid' do
-    expect(checkbox).to be_valid
+    is_expected.to be_valid
   end
 
   # All of them true because of the beautiy of Active Record <3
@@ -12,7 +12,7 @@ RSpec.describe Checkbox do
   ['true', 1, nil].each do |lol|
     it "should not be valid if it has #{lol}" do
       checkbox.air_conditioning = lol
-      expect(checkbox).to be_valid
+      is_expected.to be_valid
     end
   end
 end
